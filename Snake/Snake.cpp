2cf3e66c6796ff89dae0ae00b8705119f1be949b
Drag817 <66403146+Drag817@users.noreply.github.com>
2020-10-15 15:37:40 +0900

    add Draw function

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,11 +1,59 @@
 ﻿#include <iostream>
 
-
 using namespace std;
 
+bool gameOver;
+const int width = 20;
+const int height = 20;
+int x, y, fruitX, fruitY, score;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN };
+eDirection dir;
 
-int main()
-{
+void Setup() {
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void Draw() {
+    system("cls"); //system("clear");
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (j == 0 || j == width - 1)
+                cout << "#";
+            cout << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < width + 1; i++)
+        cout << "#";
+    cout << endl;
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
+int main() {
+    Setup();
+    while (!gameOver) {
+        Draw();
+        Input();
+        Logic();
+    }
 
     return 0;
 }
